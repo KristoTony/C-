e033826f7dd7c6a76a06035d17691fa4786e6448
KristoTony <firedfeeniz@gmail.com>
2018-06-11 13:30:33 +0400

    Add files via upload

---
+++ Casino/Default.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Image ID="Image1" runat="server" Width="139px" />
+        <asp:Image ID="Image2" runat="server" Width="139px" />
+        <asp:Image ID="Image3" runat="server" Width="139px" />
+        <br />
+        <br />
+        Your Bet:<asp:TextBox ID="TextBox1" runat="server" Width="168px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" onclick="Button1_Click" 
+            Text="Pull The Lever" />
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Result"></asp:Label>
+        <br />
+        <br />
+        Player&#39;s Money:<asp:Label ID="Label1" runat="server" Text="Money"></asp:Label>
+        <br />
+        <br />
+        <ul style=" margin-top: 0px; margin-bottom: 10px; line-height: inherit; font-weight: 400; color: rgb(51, 51, 51); font-family: &quot;Roboto Slab&quot;, serif; font-size: 16px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-style: initial; text-decoration-color: initial;">
+            <li style=" font-size: 1.07em; line-height: 2; font-weight: 400;">
+                2x Bet for 1 Cherry</li>
+            <li style=" font-size: 1.07em; line-height: 2; font-weight: 400;">
+                3x Bet for 2 Cherries</li>
+            <li style=" font-size: 1.07em; line-height: 2; font-weight: 400;">
+                4x Bet for 3 Cherries</li>
+            <li style=" font-size: 1.07em; line-height: 2; font-weight: 400;">
+                100x Bet for three 7&#39;s</li>
+        </ul>
+    
+    </div>
+    </form>
+    <p>
+        HOWEVER</p>
+    <p>
+        If There&#39;s even one BAR you win nothing.</p>
+</body>
+</html>
---
+++ Casino/Default.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            
+                double money = new double ();
+                money = 100.00;
+                ViewState.Add ("money", money);
+                Label1.Text = ViewState["money"].ToString ();
+                Image1.ImageUrl = "~/Images/" + GetImage () + ".png";//Gets the random image
+                Image2.ImageUrl = "~/Images/" + GetImage () + ".png";
+                Image3.ImageUrl = "~/Images/" + GetImage () + ".png";
+            
+        }
+    }
+
+
+    Random rand = new Random (); 
+    private string GetImage()
+    {
+        string[] images = new string[] { "Strawberry", "Bar", "Lemon", "Bell", "Clover", "Cherry", "Diamond", "Orange", "Seven", "HorseShoe", "Plum", "Watermelon" };
+        return images[rand.Next (11)];
+    }
+    
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Image1.ImageUrl = "~/Images/" + GetImage () + ".png";
+        Image2.ImageUrl = "~/Images/" + GetImage () + ".png";
+        Image3.ImageUrl = "~/Images/" + GetImage () + ".png";
+        string[] reel = new string[] { Image1.ImageUrl, Image2.ImageUrl, Image3.ImageUrl };
+        int money = int.Parse(ViewState["money"].ToString());
+        if(money > 0) Evaluvate (reel);//Calculation
+        else
+        {
+            Label1.Text = "you lost refresh the app";
+        }
+        
+     
+    }
+
+    private void Evaluvate(string[] reel)
+    {
+        int multiplier;
+        if(isBar (reel)) multiplier = 0;//checks for bar
+        else if(isJackpot (reel)) multiplier = 100;//checks for Jackpot
+        else if(isCherry (reel,out multiplier)) multiplier=multiplier/1; //checks for cherry
+        Change (multiplier);//updating the values
+    }
+
+    private bool isBar(String[] reel)
+    {
+        if(reel[0] == "~/Images/Bar.png" || reel[1] == "~/Images/Bar.png" || reel[2] == "~/Images/Bar.png")
+            return true;
+        else
+            return false;
+    }
+
+    private bool isJackpot(String[] reel)
+    {
+        if(reel[0] == "~/Images/Seven.png" && reel[1] == "~/Images/Seven.png" && reel[2] == "~/Images/Seven.png")
+            return true;
+        else
+            return false;
+    }
+
+    private bool isCherry(string[] reel,out int multiplier)
+    {
+        int count = 0;
+        for(int i = 0; i < 3; i++)
+        {
+            if(reel[i] == "~/Images/Cherry.png")
+                count++;
+        }
+        multiplier = determine (count);//determines the number of cherries
+        if(multiplier == 0)
+            return false;
+        else return true;
+    }
+
+    private int determine(int count)
+    {
+        if(count == 1) return 2;
+        else if(count == 2) return 3;
+        else if(count == 3) return 4;
+        else
+            return 0;
+
+    }
+
+    private void Change(int mult)
+    {
+        int bet = int.Parse (TextBox1.Text);
+        int money = int.Parse (Label1.Text);
+        money -= bet;
+        ViewState["money"] = money;
+        bet=bet*mult;
+        Result (bet, money);//updates the UI
+    }
+
+    private void Result(int a, int b)
+    {
+        int money = b + a;
+        ViewState["money"] = money;
+        Label2.Text = string.Format ("You won:{0:C}", a);
+        if(money > 0) Label1.Text = ViewState["money"].ToString ();
+        else Label1.Text = "You loose";
+    }
+
+}
